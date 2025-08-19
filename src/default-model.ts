ffe8794a3f525638ab4569af63379e1761f0ae4f
Oliver Benns <oliver@benns.co>
2025-08-19 12:46:54 +0700

    Fix naming

---
+++ src/default-model.ts
@@ -0,0 +1,32 @@
+export const defaultJsonModel = {
+  global: {},
+  borders: [],
+  layout: {
+    type: "row",
+    weight: 100,
+    children: [
+      {
+        type: "tabset",
+        weight: 50,
+        children: [
+          {
+            type: "tab",
+            name: "One",
+            component: "placeholder",
+          },
+        ],
+      },
+      {
+        type: "tabset",
+        weight: 50,
+        children: [
+          {
+            type: "tab",
+            name: "Two",
+            component: "placeholder",
+          },
+        ],
+      },
+    ],
+  },
+};
